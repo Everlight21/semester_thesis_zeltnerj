251ee4366d5c29128e33d8100152d7c381f1577b
Joscha Zeltner <joscha.zeltner@gmail.com>
2013-04-22 14:33:34 +0200

    220413 again stable

--- DE4_DDR2_UniPHY_zeltnerj/lvdsrx.vhd
+++ DE4_DDR2_UniPHY_zeltnerj/lvdsrx.vhd
@@ -155,7 +155,7 @@ BEGIN
 		number_of_channels => 5,
 		outclock_resource => "AUTO",
 		pll_operation_mode => "UNUSED",
-		pll_self_reset_on_loss_lock => "UNUSED",
+		pll_self_reset_on_loss_lock => "OFF",
 		port_rx_channel_data_align => "PORT_USED",
 		port_rx_data_align => "PORT_UNUSED",
 		refclk_frequency => "UNUSED",
@@ -250,7 +250,7 @@ END SYN;
 -- Retrieval info: CONSTANT: NUMBER_OF_CHANNELS NUMERIC "5"
 -- Retrieval info: CONSTANT: OUTCLOCK_RESOURCE STRING "AUTO"
 -- Retrieval info: CONSTANT: PLL_OPERATION_MODE STRING "UNUSED"
--- Retrieval info: CONSTANT: PLL_SELF_RESET_ON_LOSS_LOCK STRING "UNUSED"
+-- Retrieval info: CONSTANT: PLL_SELF_RESET_ON_LOSS_LOCK STRING "OFF"
 -- Retrieval info: CONSTANT: PORT_RX_CHANNEL_DATA_ALIGN STRING "PORT_USED"
 -- Retrieval info: CONSTANT: PORT_RX_DATA_ALIGN STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: REFCLK_FREQUENCY STRING "UNUSED"
